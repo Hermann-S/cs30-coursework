75cab6f39033d3a4c928fc8f44a77661fb90fcdf
Hermann-S <126822504+Hermann-S@users.noreply.github.com>
2023-03-03 15:22:19 -0600

    typo

--- GenerativeArt/sketch.js
+++ GenerativeArt/sketch.js
@@ -13,7 +13,7 @@ function draw() {
 
 function lotsOfLines(cols, rows) {
   for (let x = 0; x < cols; x++){
-    for (let y = 0; y , rows; y++) {
+    for (let y = 0; y < rows; y++) {
       let spaceAmount = width/cols;
       diagonalLine(x*spaceAmount, y*spaceAmount, spaceAmount);
     }
