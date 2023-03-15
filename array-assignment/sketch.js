fe1445b20064a8ce9b329efa7be1b56d33ce409f
Hermann-S <126822504+Hermann-S@users.noreply.github.com>
2023-03-15 15:19:51 -0600

    Added basic character
    basic character is in but still need to make the background and platforms as well as make weapons usable.

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -6,73 +6,82 @@
 // - describe what you did to take this project "above and beyond"
 
 let shapes = [];
-let x1;
+let x = width / 3;
 let y1;
 let dx;
 let dy;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x1 = random(height);
-  y1 = random(width);
+  // x1 = random(height);
+  // y1 = random(width);
 }
 
 function draw() {
   background(0);
-  moveShapes();
-  displayShapes();
+  translate(width / 3, height / 1.25);
+  circle(0, 0, 50);
+  fill(255);
+  // moveShapes();
+  // displayShapes();
   // circle(x1, y1, 50);
   // fill(255);
 }
 
+
 function keyPressed() {
-  if (keyCode === 65){
-    fill(255, 0, 0);
-  }
-  else if (keyCode === 68) {
-    fill(0,255,0);
-  }
-  else if (keyCode === 87) {
-    fill(255);
+  if (keyCode === 68){
+    x += 5;
   }
 }
-
-
-
-
-function mousePressed() {
-  if (dist(mouseX, mouseY, x1, y1) < 25){
-    x1 = random(width);
-    y1 = random(height);
-  }
-}
-
-// function mousePressed() {
-//   spawnBall(mouseX, mouseY);
+// function keyPressed() {
+//   if (keyCode === 65){
+//     fill(255, 0, 0);
+//   }
+//   else if (keyCode === 68) {
+//     fill(0,255,0);
+//   }
+//   else if (keyCode === 87) {
+//     fill(255);
+//   }
 // }
 
-function moveShapes() {
-  for (let i = 0; i < shapes.length; i++) {
-    shapes[i].x += shapes[i].dx;
-    shapes[i].y += shapes[i].dy;
-  }
-}
 
-function displayShapes() {
-  for ( let i = 0; i < shapes.length; i++) {
-    fill(shapes[i].theColor);
-    circle(shapes[i].x, shapes[i].y, shapes[i].diameter);
-  }
-}
 
-function spawnBall(tempX,tempY) {
-  let newBall = {
-    x: tempX,
-    y: tempY,
-    dx: random(-5, 5),
-    dy: random(-5, 5),
-    diameter: random(25, 100),
-    theColor: color(255)
-  };
-  shapes.push(newBall);
-}
+
+// function mousePressed() {
+//   if (dist(mouseX, mouseY, x1, y1) < 25){
+//     x1 = random(width);
+//     y1 = random(height);
+//   }
+// }
+
+// // function mousePressed() {
+// //   spawnBall(mouseX, mouseY);
+// // }
+
+// function moveShapes() {
+//   for (let i = 0; i < shapes.length; i++) {
+//     shapes[i].x += shapes[i].dx;
+//     shapes[i].y += shapes[i].dy;
+//   }
+// }
+
+// function displayShapes() {
+//   for ( let i = 0; i < shapes.length; i++) {
+//     fill(shapes[i].theColor);
+//     circle(shapes[i].x, shapes[i].y, shapes[i].diameter);
+//   }
+// }
+
+// function spawnBall(tempX,tempY) {
+//   let newBall = {
+//     x: tempX,
+//     y: tempY,
+//     dx: random(-5, 5),
+//     dy: random(-5, 5),
+//     diameter: random(25, 100),
+//     theColor: color(255)
+//   };
+//   shapes.push(newBall);
+// }
